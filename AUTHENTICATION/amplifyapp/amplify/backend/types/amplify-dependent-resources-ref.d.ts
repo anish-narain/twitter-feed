0e1364aadb2322bb8d5447d9586b92a58ded2465
Riya050302 <riya.chard@hotmail.co.uk>
2024-01-22 00:19:59 +0000

    authentification complete

---
+++ AUTHENTICATION/amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "twitter57f0f3d4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
