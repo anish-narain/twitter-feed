b13443fcbfc36f69a80f3189fa80f7c7d8ce396c
anish-narain <69715492+anish-narain@users.noreply.github.com>
2024-01-29 23:09:04 +0000

    updating server

--- Main_FullStack/amplifyapp/Backend/w_mm_testing_sensor_table.js
+++ Main_FullStack/amplifyapp/Backend/w_mm_testing_sensor_table.js
@@ -11,6 +11,7 @@ AWS.config.update({
     region: "us-east-1"
 });
 
+const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const tableName = "Twitter_weight_table";
 const bucketName = "idkw";  // Replace with your actual bucket name
