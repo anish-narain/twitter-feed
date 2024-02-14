4222c166973f95b4fec70ca6f7f6ecb74c8040ec
Awais Khawaja <62144939+awaiskhawaja@users.noreply.github.com>
2024-02-14 12:46:55 +0000

    Update main.py

--- Raspberry pi/actual upload/main.py
+++ Raspberry pi/actual upload/main.py
@@ -6,9 +6,9 @@ i2c_bus = smbus2.SMBus(1)
 
 weight1 = weightSensor.ADC(0x48, i2c_bus) # ADC on address 0x48
 weight1.setConfig("100", "001", "0", write=True) # Set voltage range to smallest, i.e. most sensitive. Set to continuous read mode, so need to write new config now.
-weightSensor.calibrateSensors(weight1, 231)
+weightSensor.calibrateSensors(weight1, mass=231, mass2=369)
 
 while True:
-    (w, w_g, detected) = weightSensor.loop(weight1, 25)
-    #print(w_g, weightSensor.measure2_g(weight1))
-    #print(tempSensor.readTemp(i2c_bus))
+    (w, w_g, bird_detect, capture_time, image_file_name, file_path) = weightSensor.loop(weight1, 80)
+    w2 = weightSensor.measure2_g(weight1)
+    print(w_g, w2, tempSensor.readTemp(i2c_bus))
